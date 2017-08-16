07b5761cd06660bcd7feba30caa7656612f37188
kilka <koyokin2013@yandex.ru>
2017-08-16 16:30:28 +0300

    Добавил возможность переходить по ссылочкам (кроме редактора).

--- web_side/app/views.py
+++ web_side/app/views.py
@@ -1,14 +1,17 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template("index.html")
 
+
 @app.route('/sign_in')
 def sign_in():
 	return render_template("sign_in.html")
+
 
 @app.route('/sign_up')
 def sign_up():
